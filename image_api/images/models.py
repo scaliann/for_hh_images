75130e3d70096437f5ca97dc97ac254da350821c
scaliann <mishah238@gmail.com>
2024-10-26 13:50:05 +0300

    GrayImage, resize100, resize500

--- image_api/images/models.py
+++ image_api/images/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 class Image(models.Model):
     name = models.CharField(max_length=255)
-    file_path = models.ImageField(upload_to='images/')
+    file_path = models.ImageField(upload_to='images/media/original')
+    resized_100_path = models.ImageField(upload_to='images/media/resized_100/', blank=True, null=True)
+    resized_500_path = models.ImageField(upload_to='images/media/resized_500/', blank=True, null=True)
     upload_date = models.DateTimeField(auto_now_add=True)
-    resolution = models.CharField(max_length=50)
-    size = models.IntegerField()
+    resolution = models.CharField(max_length=50, blank=True)
+    size = models.IntegerField(blank=True)
+    format = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.name
--- image_api/images/views.py
+++ image_api/images/views.py
@@ -2,8 +2,53 @@ from rest_framework import viewsets
 from .models import Image
 from .serializers import ImageSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
+from PIL import Image as PILImage, ImageOps
+from io import BytesIO
+from django.core.files.base import ContentFile
+
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
     parser_classes = [MultiPartParser, FormParser]
+
+    def perform_create(self, serializer):
+
+        instance = serializer.save()
+
+        pil_image = PILImage.open(instance.file_path)
+        gray_image = ImageOps.grayscale(pil_image)
+
+        instance.size = gray_image.size[0] * gray_image.size[1]
+        instance.resolution = f"{gray_image.width}x{gray_image.height}"
+        instance.format = gray_image.format or pil_image.format
+
+        gray_image_file = BytesIO()
+        gray_image.save(gray_image_file, format=instance.format)
+        instance.file_path.save(f"{instance.name}_gray.{instance.format.lower()}",
+                                ContentFile(gray_image_file.getvalue()), save=False)
+
+
+        resized_100 = gray_image.resize((100, 100))
+        resized_500 = gray_image.resize((500, 500))
+
+
+        resized_100_file = BytesIO()
+        resized_500_file = BytesIO()
+        resized_100.save(resized_100_file, format=instance.format)
+        resized_500.save(resized_500_file, format=instance.format)
+
+        instance.resized_100_path.save(f"{instance.name}_100x100.{instance.format.lower()}",
+                                       ContentFile(resized_100_file.getvalue()), save=False)
+        instance.resized_500_path.save(f"{instance.name}_500x500.{instance.format.lower()}",
+                                       ContentFile(resized_500_file.getvalue()), save=False)
+
+
+        gray_image_file.close()
+        resized_100_file.close()
+        resized_500_file.close()
+
+
+        instance.save()
